96ba0e62b3712d03a64387bbec0948787118dd20
Arggonaut <and123zhe@gmail.com>
2026-03-21 16:02:02 -0400

    moved some logic to a separate convertImageToAscii function and added logic to resize image

--- main.c
+++ main.c
@@ -2,8 +2,11 @@
 #include "stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
+#define STB_IMAGE_RESIZE_IMPLEMENTATION
+#include "stb_image_resize2.h"
 #include <stdio.h>
 #include <string.h>
+#include "main.h"
 
 const int MAX_BRIGHTNESS = 255; //the maximum value for a 8-bit color channel
 const char DENSITY_CHARS[] = "_.=+:;!?c71236089$W#@"; //a list of characters based on the amount of space they take up
@@ -11,22 +14,34 @@ const double DENSITY_CHAR_RANGE = MAX_BRIGHTNESS / (float) (strlen(DENSITY_CHARS
 
 int main(void) {
     //Load the image
-    int width = 0; //declare parameters for the image and set them to 0 for now
-    int height = 0;
-    int channels = 0; 
+    int imageWidth, imageHeight, channels = 0; //declare parameters for the image and set them to 0 for now
     char imageName[] = "64x64_Gradient_Horizontal.jpg"; //declare and set imageName string to the name of the image file
-    unsigned char *image = stbi_load(imageName, &width, &height, &channels, 0); //load the image using stb_image
+    unsigned char *image = stbi_load(imageName, &imageWidth, &imageHeight, &channels, 0); //load the image using stb_image
     if (image == NULL) { //if image is NULL, loading failed
-        printf("Error in loading image\n"); //return an error message
+        printf("Error in loading image\n"); //print an error message
         exit(1); //close with an error status
     }
 
-    //initialize and set imageSize
-    size_t imageSize = width * height * channels;
+    //resize the image to what we want the output ascii art's dimensions to be
+    int width = 128;               //will be changed when gui gets implemented
+    int height = (int) width / 2.5;    
+    unsigned char* resizedImage = stbir_resize_uint8_srgb(image,  imageWidth,  imageHeight,  0, 
+                                                        NULL, width, height, 0, channels);
+    if (resizedImage == NULL) {
+        printf("Error in resizing image\n");
+        exit(1);
+    }
 
+    
+    //the original image malloc is no longer needed so free it and set the pointer to NULL
+    stbi_image_free(image);
+    image = NULL;
+    
+    //initialize and set imageSize for when we loop through pixels
+    size_t imageSize = width * height * channels; 
 
     //initialize the output array of strings
-    char outputAscii[height][width + 1];
+    char outputAscii[height][width + 1]; 
     //loop through 2D array and set chars to null terminator
     for (int row = 0; row < height; row++) { 
         for (int column = 0; column < width; column++) {
@@ -35,7 +50,20 @@ int main(void) {
         outputAscii[row][width] = 0;
     }
 
+    //turn the resized image into ascii
+    convertImageToAscii(resizedImage, imageSize, channels, width, outputAscii);
 
+    //loop through each row of the outputAscii array and print it out
+    for (int i = 0; i < height; i++) {
+        printf("%s\n", outputAscii[i]);
+    }
+
+
+    return 0; //return 0 for success
+}
+
+//take an image a corrosponding 2D array of chars and turn the array into the ascii version of the image
+void convertImageToAscii(unsigned char *image, size_t imageSize, int channels, int width, char outputAscii[][width + 1]) {
     //declare row and column index ints for looping through the 2D array and set them to 0
     int row = 0;
     int column = 0;
@@ -57,14 +85,4 @@ int main(void) {
         //increment column as we move on to the next pixel
         column++;
     }
-
-    //loop through each row of the outputAscii array and print it out
-    for (int i = 0; i < height; i++) {
-        printf("%s\n", outputAscii[i]);
-    }
-
-    //free image from the heap and set it to NULL
-    stbi_image_free(image);
-    image = NULL;
-    return 0;
 }
